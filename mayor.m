87d74ce1526a61f55c861fd939754dc15be39087
jcjimenezb123 <55670828+jcjimenezb123@users.noreply.github.com>
2019-10-19 14:22:03 -0500

    Add files via upload

---
+++ mayor.m
@@ -0,0 +1,15 @@
+a=input('1er valor ');
+b=input('2do valor ');
+c=input('3er valor ');
+if a>b
+    if a>c
+        disp('el mayor es el 1er valor'); %a
+    else
+        disp('el mayor es el 3er valor'); %c
+    end
+elseif b>c
+    disp('el mayor es el 2do. valor');%b
+else
+    disp('el mayor es el 3er valor');%c
+end
+    
---
+++ sicondicional.asv
@@ -0,0 +1,10 @@
+cal=input('Calificacion');
+if cal>5
+    if cal==10
+        disp('Felicidades')
+    else
+        disp('Aprobado');
+    end
+else
+    disp('Reprobado');
+end
